6ee846a18bb56bfe93eefbea21a05feffba8fb93
Damien Zarich <damienzarich@gmail.com>
2026-06-27 05:58:48 -0700

    added more images finsihed script to make images on side also enlarge, and made images go from right to left.

--- function.js
+++ function.js
@@ -1,10 +1,15 @@
 const images = document.querySelectorAll('.images img');
 images.forEach((img) => {
-    img.addEventListener('mouseEnter', (e) => {
-        const current = 
+    img.addEventListener('mouseenter', (e) => {
+        const current = e.target;
+        const prev = current.previousElementSibling;
+        const next = current.nextElementSibling;
+        if (prev) prev.classList.add('neighbor');
+        if (next) next.classList.add('neighbor');
     })
-})
 
-img.addEventListener('mouseLeave', (e) => {
+
+img.addEventListener('mouseleave', (e) => {
     images.forEach(i => i.classList.remove('neighbor'));
+ });
 });
