407e6f4cbe07f1ea56a298c52d21be054620e201
Damien Zarich <damienzarich@gmail.com>
2026-06-27 05:12:01 -0700

    adding js to make the images on side of your selected image become slightyl dialated

---
+++ function.js
@@ -0,0 +1,10 @@
+const images = document.querySelectorAll('.images img');
+images.forEach((img) => {
+    img.addEventListener('mouseEnter', (e) => {
+        const current = 
+    })
+})
+
+img.addEventListener('mouseLeave', (e) => {
+    images.forEach(i => i.classList.remove('neighbor'));
+});
