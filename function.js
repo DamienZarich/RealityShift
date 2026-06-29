e89ac8ac1f00f3d00e3684d4c7ed8587d95a7ced
Damien Zarich <damienzarich@gmail.com>
2026-06-29 09:08:23 -0700

    adding js for the hamburger menu

--- function.js
+++ function.js
@@ -3,4 +3,11 @@ document.addEventListener("DOMContentLoaded", () => {
     const photoTitle = document.getElementById("photo-title")
     const thumbs = document.querySelectorAll(".thumb-items")
     const showcase = document.getElementById("showcase-box")
-})
+
+    let currentIndex = 0;
+    let animationId = null;
+    const CYCLE_DURATION = 10000;
+
+    const hamburger = document.getElementById("hamburger");
+    const 
+});
