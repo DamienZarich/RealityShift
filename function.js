7833e85a0c934deb05c19737f59e81f559a48508
Damien Zarich <damienzarich@gmail.com>
2026-06-29 09:05:21 -0700

    declaring items in js and getting them from my html

---
+++ function.js
@@ -0,0 +1,6 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const mainDisplay = document.getElementById("main-video");
+    const photoTitle = document.getElementById("photo-title")
+    const thumbs = document.querySelectorAll(".thumb-items")
+    const showcase = document.getElementById("showcase-box")
+})
